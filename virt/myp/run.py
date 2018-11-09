0c102bc97ef7969df7be87f4ee798a1c6297d1ac
Rohan kedambadi <31340440+RohanKedambadi98@users.noreply.github.com>
2018-11-09 09:58:50 +0530

    my 1st python project

---
+++ virt/myp/run.py
@@ -0,0 +1,17 @@
+# run.py
+
+# Add your settings here... this is a temporary location, as the settings for a Flask app
+# should be stored separate from your main program.
+DEBUG = True
+
+from project import app
+
+if __name__ == "__main__":
+    import os
+
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT,debug='true')
